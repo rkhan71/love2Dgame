1c58a2809ada142636a735135a91418597d972aa
Rayan Khan <rayan.ahkhan@gmail.com>
2022-07-10 13:25:06 +0300

    adds sound and a game over screen

--- harvest_havoc/main.lua
+++ harvest_havoc/main.lua
@@ -5,7 +5,6 @@ function love.load()
 
     world = love.physics.newWorld(0, 0, false)
 
-    
     basket = {}
     basket.body = love.physics.newBody(world, (ww / 2), wh - 50, 'kinematic')
     basket.shape = love.physics.newRectangleShape(100, 100)
@@ -21,6 +20,13 @@ function love.load()
     tri.shape = love.physics.newPolygonShape(0, -25, 25, 10, -25, 10)
     tri.fixture = love.physics.newFixture(tri.body, tri.shape)
     tri.body:setFixedRotation(true)
+
+    loadscreen = love.audio.newSource('loadscreen.mp3', 'stream')
+    playing = love.audio.newSource('play.mp3', 'stream')
+    life = love.audio.newSource('life.mp3', 'static')
+    gameover = love.audio.newSource('gameover.mp3', 'static')
+    change = love.audio.newSource('change.mp3', 'static')
+    gain = love.audio.newSource('gain.mp3', 'static')
 
     function reset()
         circle.body:setPosition(love.math.random(25, ww - 25), -25)
@@ -44,10 +50,17 @@ function love.load()
     reset()
 
     play = false
+    loser = false
 end
 
 function love.update(dt)
     if play then
+        love.audio.stop(loadscreen)
+
+        if not playing:isPlaying() then
+            love.audio.play(playing)
+        end
+
         world:update(dt)
 
         time = time - dt
@@ -83,9 +96,11 @@ function love.update(dt)
         if basket.body:isTouching(circle.body) and (cx >= x - 50) and (cx <= x + 50) then
             if fruit == 'circle' then
                 score = score + 1
+                love.audio.play(gain)
                 count = 0
             else
                 lives = lives - 1
+                love.audio.play(life)
             end
             circle.body:setPosition(love.math.random(25, ww - 25), -25)
         end
@@ -93,9 +108,11 @@ function love.update(dt)
         if basket.body:isTouching(tri.body) and (tx >= x - 50) and (tx <= x + 50) then
             if fruit == 'triangle' then
                 score = score + 1
+                love.audio.play(gain)
                 count = 0
             else
                 lives = lives - 1
+                love.audio.play(life)
             end
             tri.body:setPosition(love.math.random(25, ww - 25), -10)
         end
@@ -116,6 +133,7 @@ function love.update(dt)
 
         if count == 3 then
             lives = lives - 1
+            love.audio.play(life)
         end
 
         if lives == 0 then
@@ -123,18 +141,30 @@ function love.update(dt)
                 love.filesystem.write('highscore.txt', score)
             end
             play = false
+            love.audio.stop(playing)
+            loser = true
             reset()
         end
 
         if time <= 0 then
+            love.audio.play(change)
             time = love.math.random(3,6)
             fruit = fruits[love.math.random(2)]
         end
+    elseif loser then
+        if not life:isPlaying() then
+            love.audio.play(gameover)
+            love.timer.sleep(5)
+            loser = false
+        end
     else
         function love.keypressed(key)
-            if key == 'space' then
+            if key == 'space' and loser == false then
                 play = true
             end
+        end
+        if not loadscreen:isPlaying() and not gameover:isPlaying() then
+            love.audio.play(loadscreen)
         end
     end
 end
@@ -145,6 +175,8 @@ function love.draw()
         love.graphics.polygon('fill', tri.body:getWorldPoints(tri.shape:getPoints()))
         love.graphics.circle('fill', circle.body:getX(), circle.body:getY(), circle.shape:getRadius())
         love.graphics.print('Score: '..score..'\nLives: '..lives..'\nCount: '..count..'\nHarvest: '..fruit, 15, 15)
+    elseif loser then
+        love.graphics.print('GAME OVER', ww / 2, wh / 2)
     else
         love.graphics.print('Press Spacebar to Start\nHighscore: '..highscore, 15, 15)
     end
